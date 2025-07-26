2c69e3caff1b55a548fbf49cd86b326317d193f5
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-07-27 05:04:49 +0530

    wrangler login

--- src/index.ts
+++ src/index.ts
@@ -15,15 +15,8 @@ export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		console.log(request.body);
 		console.log(request.headers);
-
-		if (request.method === 'GET') {
-			return Response.json({
-				message: 'you sent a get request',
-			});
-		} else {
-			return Response.json({
-				message: 'you did not send a get request',
-			});
-		}
+		return Response.json({
+			message: 'hdllo testing cloudflare',
+		});
 	},
 };
